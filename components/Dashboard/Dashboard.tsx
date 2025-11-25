afc955b8f1a59c5725664983e86060c56cd0712a
Alyssa Favorito <alyssafavorito2000@gmail.com>
2025-11-25 17:56:16 -0500

    Firebase configuration, dashboard component creation

---
+++ components/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from "react";
+import styles from "./Dashboard.module.scss";
+
+export default function Dashboard () {
+    return (
+        <div className={styles.dashboard}>
+            <div className={styles.tasks}>
+                
+            </div>
+        </div>
+    )
+}
