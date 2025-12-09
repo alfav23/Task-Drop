ddcabe882619ec02558aec199392859de9768a2a
Alyssa Favorito <alyssafavorito2000@gmail.com>
2025-12-09 18:03:39 -0500

    fixed logout on refresh issue (implemented onAuthStateChanged)

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,37 @@ import styles from "./page.module.css";
 import Dashboard from "@/components/Dashboard";
 import { useAuth } from "@/context/AuthContext";
 // import { auth } from "firebase-admin";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const router = useRouter();
   const auth = getAuth();
-  const user = auth.currentUser;
-  const { loading } = useAuth();
+  const [ user, setUser ] = useState(auth.currentUser);
+  const [ loading, setLoading ] = useState(true);
   console.log(user);
 
   useEffect(() => {
+    onAuthStateChanged(auth, (activeUser) => {
+      if (activeUser) {
+        setUser(activeUser);
+        console.log(user);
+      } else {
+        console.log("no active user")
+      }
+        setLoading(false);
+    })
+  }, [auth]);
+  
+  useEffect(() => {
     // Only redirect after auth has finished initializing
-    if (!loading && !user || user === null) {
-      router.push('/login');
+    if (!loading) {
+      if (!user || user === null) {
+        router.push('/login');
+      }
     }
-  }, []);
+  }, [loading]);
 
   if (user){
     return (
@@ -42,5 +56,4 @@ export default function Home() {
     return null;
   }
 }
-  
-  
+
